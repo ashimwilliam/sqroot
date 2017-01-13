a5fa783a801630cdff37e3026be8ad20e5ee5f90
ashimwilliam <ashimwilliam@gmail.com>
2017-01-13 17:24:06 +0530

    according to pankaj changes

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   layout 'devise'
 
-puts "+++++++++++++++++++++++++++++++++++++++++++++" 
-
 # before_action :configure_sign_up_params, only: [:create]
 # before_action :configure_account_update_params, only: [:update]
 
